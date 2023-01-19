734082f5d4d3bcfb18791b3155e1fc87f06fa471
Akad-samy <akhouad.samy@gmail.com>
2023-01-19 02:03:27 +0100

    new composer

--- src/PhoneNumber.php
+++ src/PhoneNumber.php
@@ -8,29 +8,29 @@ class PhoneNumber
 {
     private $phoneNumber;
 
-    public function __construct()
+    public function __construct(PhoneNumberInterface $phoneNumber)
     {
+        $this->phoneNumber = $phoneNumber;
     }
 
     public static function standardizePhoneNumber(string $phoneNumber, ?string $type = 'local', ?string $country = 'ma')
     {
+        if(!$phoneNumber){
+            throw new Exception("phone number is required", 400);
+        }
+
+        if(!in_array($type, ['local', 'international'])){
+            throw new Exception("type must be local or international", 400);
+        }
+
+        if(!in_array($country, ['ma', 'fr'])){
+            throw new Exception("country must be 'ma' or 'fr'", 400);
+        }
+
+        if(!($this->phoneNumber->verifyPhoneNumber($phoneNumber))){
+            throw new Exception("invalid phone number", 400);
+        }
         return 'hello world';
-        // if(!$phoneNumber){
-        //     throw new Exception("phone number is required", 400);
-        // }
-
-        // if(!in_array($type, ['local', 'international'])){
-        //     throw new Exception("type must be local or international", 400);
-        // }
-
-        // if(!in_array($country, ['ma', 'fr'])){
-        //     throw new Exception("country must be 'ma' or 'fr'", 400);
-        // }
-
-        // if(!($this->phoneNumber->verifyPhoneNumber($phoneNumber))){
-        //     throw new Exception("invalid phone number", 400);
-        // }
-        // return 'hello world';
 
     }
 }
